5748ea9b479388faa250f22f026bf0a3ba0458df
glauberbrack <glauber@brack.com.br>
2022-05-17 00:01:10 -0300

    feat: mocked tests with nock

--- __tests__/screens/User/index.tsx
+++ __tests__/screens/User/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react-native';
+import nock from 'nock';
 import User from '../../../src/screens/User';
 
 jest.useFakeTimers();
+
+// The ideia of nock is: any request to this specific endpoint
+// will ALWAYS returns to me the same response.
+// We can return any status code and property as we desire.
+nock('https://api.github.com').get('/users/glauberbrack').reply(200, {
+  name: 'Glauber Brack',
+});
 
 describe('render successfully', () => {
   it('call github api on page load', async () => {
